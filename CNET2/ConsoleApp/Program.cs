f6312921dc58154a51ff8e76f4e2790a69f05af4
pavel.malina <pam369@seznam.cz>
2022-06-13 12:07:04 +0200

    inicializace dictionary

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -1,3 +1,11 @@
-﻿
+﻿using Model;
+
 Console.WriteLine("Hello, World!");
 
+var fAResult = new FAResult
+{
+    Source = "file",
+    SourceType = SourceType.File,
+};
+
+Console.WriteLine(fAResult);
--- CNET2/Model/FAResult.cs
+++ CNET2/Model/FAResult.cs
@@ -1,11 +1,32 @@
 ﻿namespace Model
 {
+    public enum SourceType
+    {
+        Url,
+        File
+    }
+
     /// <summary>
     /// výsledek frekvenční analýzy pro jeden zdroj
     /// soubor / url
     /// </summary>
     public class FAResult
     {
+        /// <summary>
+        /// Zdroj textu.
+        /// </summary>
+        public string Source { get; set; }
 
+        /// <summary>
+        /// Typ zdroje.
+        /// </summary>
+        public SourceType SourceType { get; set; }
+
+        /// <summary>
+        /// Výsledná frekvenční analýza slov.
+        /// </summary>
+        public Dictionary<string, int> Words { get; set; } = new Dictionary<string, int>();
+
+        public override string ToString() => $"{SourceType} / {Source} - pocet: {Words?.Count}";
     }
 }
