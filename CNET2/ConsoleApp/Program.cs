cc88de8a2683fd02ffd773fd12eead0482d343e1
pavel.malina <pam369@seznam.cz>
2022-06-13 16:29:27 +0200

    aa

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -19,25 +19,26 @@ var absValue = numbers.Select(x => Math.Abs(x));
 printNuAr(absValue);
 
 // Ukol 1
+Console.WriteLine();
+
 var ukol1 = numbers.Count(x => x > 0);
-Console.WriteLine(ukol1);
+Console.WriteLine($"Ukol 1: {ukol1}");
 
 // Ukol 2
 var ukol2 = numbers.OrderBy(x => x).Skip(1).SkipLast(1).Average();
-Console.WriteLine("Ukol2:");
-Console.WriteLine(ukol2);
+Console.WriteLine($"Ukol 2: {ukol2}");
 
 // Ukol 3
 var ukol3s = numbers.Where(x => x % 2 == 0).Count();
 var ukol3l = numbers.Where(x => x % 2 != 0).Count();
-Console.WriteLine($"Ukol3: suda: {ukol3s} licha: {ukol3l}");
+Console.WriteLine($"Ukol 3: suda: {ukol3s} licha: {ukol3l}");
 
 // Ukol 4
 var numbers2 = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-var strings = new[] { "zero", "one", "two", "three", "four",
-    "five", "six", "seven", "eight", "nine" };
+var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+Console.WriteLine($"Ukol 4: {string.Join(", ", numbers2.Select(x => strings[x]))}");
 
-Console.WriteLine(string.Join(", ", numbers2.Select(x => strings[x])));
+
 
 
 void printNuAr(IEnumerable<int> numbers)
