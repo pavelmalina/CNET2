14e148e58c06d1383503f4d4b8b74918a05baba3
pavel.malina <pam369@seznam.cz>
2022-06-13 14:25:21 +0200

    Funkcni verze

---
+++ CNET2/Data/FreqAnalysis.cs
@@ -0,0 +1,40 @@
+﻿namespace Data
+{
+    public class FreqAnalysis
+    {
+        public static Dictionary<string, int> FreqAnalysisFromString(string input)
+        {
+            var result = new Dictionary<string, int>();
+            var strAr = input.Split(new String[] { " ", "\t", "\r", "\n", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in strAr)
+            {
+                if (result.ContainsKey(item))
+                {
+                    result[item]++;
+                }
+                else
+                {
+                    result.Add(item, 1);
+                }
+            }
+
+            return result;
+        }
+
+        public static async Task<Dictionary<string, int>> FreqAnalysisFromUrlAsync(string url)
+        {
+            var client = new HttpClient();
+            var content = await client.GetStringAsync(url);
+
+            return FreqAnalysisFromString(content);
+        }
+
+        public static Dictionary<string, int> FreqAnalysisFromFile(string fileName)
+        {
+            var content = File.ReadAllText(fileName);
+
+            return FreqAnalysisFromString(content);
+        }
+    }
+}
